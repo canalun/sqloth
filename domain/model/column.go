a899dc78e93fb382dc98bb61bf15a48122bee35f
canalun <kanakanarunrun@gmail.com>
2022-06-09 00:39:16 +0900

    imp method for generate values with constraints reccursively using column graph

--- domain/model/column.go
+++ domain/model/column.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"errors"
+	"strconv"
 )
 
 // TODO: reorder and reorganize source code
@@ -94,6 +95,21 @@ func (c *Column) SetConstraint(constraint Constraint) {
 	c.Constraint = constraint
 }
 
+func (c Column) GenerateData(n int) []Value {
+	d := []Value{}
+	switch c.AutoIncrement {
+	case true:
+		for i := 0; i < n; i++ {
+			d = append(d, Value(strconv.Itoa(i)))
+		}
+	default:
+		for i := 0; i < n; i++ {
+			d = append(d, Value(c.GenerateRandomData()))
+		}
+	}
+	return d
+}
+
 func (c Column) GenerateRandomData() ColumnData {
 	var data string
 	switch c.Type.Base {
@@ -109,4 +125,74 @@ func (c Column) GenerateRandomData() ColumnData {
 		data = generateRandomJson()
 	}
 	return ColumnData(data)
+}
+
+//TODO: better to be defined as a method of map[ColumnFullName][]Value?
+//TODO: shuffle values. currently, values with constraints are just simple sum of strings in the order.
+func GenerateValuesForColumns(cg ColumnGraph, n int) map[ColumnFullName][]Value {
+	dict := map[ColumnFullName][]Value{}
+	for i := range cg.ColumnNodes {
+		if !cg.isAllDone() {
+			generateValuesForColumnsByRecursion(&cg, i, n, dict)
+		}
+	}
+	return dict
+}
+
+//TODO: better to be defined as a method with side-effect of map[ColumnFullName][]Value?
+func generateValuesForColumnsByRecursion(cg *ColumnGraph, i, n int, dict map[ColumnFullName][]Value) {
+	if cg.ColumnNodes[i].isDone {
+		return
+	}
+
+	//TODO: error handling
+	hasParentNodes, _ := cg.HasParentNodes(i)
+	switch hasParentNodes {
+	case false:
+		c := cg.ColumnNodes[i].GetColumn()
+		d := c.GenerateData(n)
+		dict[c.FullName] = d
+		cg.ColumnNodes[i].Done()
+		if hasChildrenNodes, _ := cg.HasChildrenNodes(i); hasChildrenNodes {
+			childrenNodesIndexes, _ := cg.ChildrenNodeIndexes(i)
+			for _, childrenNodeIndex := range childrenNodesIndexes {
+				if allDone, _ := cg.IsParentNodesAreAllDone(childrenNodeIndex); allDone {
+					generateValuesForColumnsByRecursion(cg, childrenNodeIndex, n, dict)
+				}
+			}
+		}
+	default:
+		allDone, _ := cg.IsParentNodesAreAllDone(i)
+		switch allDone {
+		case true:
+			//TODO: distinct values and data
+			values := []Value{}
+			for j := 0; j < n; j++ {
+				d := ""
+				parentNodeIndexes, _ := cg.ParentNodeIndexes(i)
+				for _, parentNodeIndex := range parentNodeIndexes {
+					fn := cg.ColumnNodes[parentNodeIndex].GetColumn().FullName
+					d += string(dict[fn][j])
+				}
+				values = append(values, Value(d))
+			}
+			dict[cg.ColumnNodes[i].GetColumn().FullName] = values
+			cg.ColumnNodes[i].Done()
+			if hasChildrenNodes, _ := cg.HasChildrenNodes(i); hasChildrenNodes {
+				childrenNodesIndexes, _ := cg.ChildrenNodeIndexes(i)
+				for _, childrenNodeIndex := range childrenNodesIndexes {
+					if allDone, _ := cg.IsParentNodesAreAllDone(childrenNodeIndex); allDone {
+						generateValuesForColumnsByRecursion(cg, childrenNodeIndex, n, dict)
+					}
+				}
+			}
+		default:
+			parentNodeIndexes, _ := cg.ParentNodeIndexes(i)
+			for _, parentIndex := range parentNodeIndexes {
+				if !cg.ColumnNodes[parentIndex].IsDone() {
+					generateValuesForColumnsByRecursion(cg, parentIndex, n, dict)
+				}
+			}
+		}
+	}
 }
--- domain/model/column_test.go
+++ domain/model/column_test.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -39,6 +40,163 @@ func TestStrToColumnTypeBase(t *testing.T) {
 			if diff != "" {
 				t.Error("-:got, +:want", diff)
 			}
+		})
+	}
+}
+
+func TestColumn_GenerateData(t *testing.T) {
+	type fields struct {
+		Name          ColumnName
+		FullName      ColumnFullName
+		Type          ColumnType
+		AutoIncrement bool
+		Constraint    Constraint
+	}
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   []Value
+	}{
+		{
+			name: "return int slice {0...n} when AutoIncrement is true",
+			fields: fields{
+				Name:     "test",
+				FullName: "test",
+				Type: ColumnType{
+					Base: Int,
+				},
+				AutoIncrement: true,
+				Constraint:    Constraint{},
+			},
+			args: args{n: 3},
+			want: []Value{Value(strconv.Itoa(0)), Value(strconv.Itoa(1)), Value(strconv.Itoa(2))},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := Column{
+				Name:          tt.fields.Name,
+				FullName:      tt.fields.FullName,
+				Type:          tt.fields.Type,
+				AutoIncrement: tt.fields.AutoIncrement,
+				Constraint:    tt.fields.Constraint,
+			}
+			got := c.GenerateData(tt.args.n)
+			diff := cmp.Diff(got, tt.want)
+			if diff != "" {
+				t.Error("Column.GenerateData(); -:got, +:want", diff)
+			}
+		})
+	}
+}
+
+func TestGenerateValuesForColumns(t *testing.T) {
+	type args struct {
+		cg ColumnGraph
+		n  int
+	}
+	tests := []struct {
+		name     string
+		args     args
+		assertFn func(map[ColumnFullName][]Value)
+	}{
+		{
+			name: "return map of values for columns considering foreign key constraints",
+			args: args{
+				cg: ColumnGraph{
+					AdjacencyMatrix: AdjacencyMatrix{
+						{0, 0, 0, 1},
+						{0, 0, 0, 0},
+						{0, 0, 0, 0},
+						{0, 1, 1, 0},
+					},
+					ColumnNodes: []ColumnNode{
+						ColumnNode{
+							column: Column{
+								FullName: "test0",
+								Type: ColumnType{
+									Base:  Int,
+									Param: ColumnTypeParam(1), //TODO: if the given schema is wrong(e.g. the param for column test0 was Int(3)), should sqloth validate it and notice users the inconsistency?
+								},
+							},
+							isDone: false,
+							index:  0,
+						},
+						ColumnNode{
+							column: Column{
+								FullName: "test1",
+								Type: ColumnType{
+									Base:  Int,
+									Param: ColumnTypeParam(1),
+								},
+							},
+							isDone: false,
+							index:  1,
+						},
+						ColumnNode{
+							column: Column{
+								FullName: "test2",
+								Type: ColumnType{
+									Base:  Int,
+									Param: ColumnTypeParam(1),
+								},
+							},
+							isDone: false,
+							index:  2,
+						},
+						ColumnNode{
+							column: Column{
+								FullName: "test3",
+								Type: ColumnType{
+									Base:  Int,
+									Param: ColumnTypeParam(2),
+								},
+							},
+							isDone: false,
+							index:  3,
+						},
+					},
+				},
+				n: 3,
+			},
+			assertFn: func(m map[ColumnFullName][]Value) {
+				for key, vs := range m {
+					switch key {
+					case "test0":
+						for idx, v := range vs {
+							if v != m["test3"][idx] {
+								t.Errorf("values of test1 is not valid; idx: %v, value: %v", idx, v)
+							}
+						}
+					case "test1", "test2":
+						for idx, v := range vs {
+							n, err := strconv.Atoi(string(v))
+							if err != nil {
+								t.Errorf("cannot convert value to int; value: %v", v)
+							}
+							if n < 0 || n > 9 {
+								t.Errorf("values of %v is out of range; idx: %v, value: %v", key, idx, v)
+							}
+						}
+					case "test3":
+						for idx, v := range vs {
+							if v != m["test1"][idx]+m["test2"][idx] {
+								t.Errorf("values of test3 is not valid; idx: %v, value: %v", idx, v)
+							}
+						}
+					}
+				}
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := GenerateValuesForColumns(tt.args.cg, tt.args.n)
+			tt.assertFn(got)
 		})
 	}
 }
