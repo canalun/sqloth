933e1c1d7cc5ceceb9efa0fd299ed58e16b93e78
canalun <kanakanarunrun@gmail.com>
2022-06-14 02:56:50 +0900

    mod query template and rm `\n`

--- domain/model/queries.go
+++ domain/model/queries.go
@@ -4,16 +4,16 @@ func GenerateQuery(rft map[TableName][]Record) []string {
 	if len(rft) == 0 {
 		return []string{}
 	}
-	re := []string{"SET FOREIGN KEY = 0\n"}
+	re := []string{"SET FOREIGN KEY = 0;"}
 	for tableName, records := range rft {
 		q := "INSERT INTO " + string(tableName) + " VALUES "
 		for _, record := range records {
 			q += querizeRecord(record)
 		}
-		q = q[:len(q)-1] + ";\n"
+		q = q[:len(q)-1] + ";"
 		re = append(re, q)
 	}
-	re = append(re, "SET FOREIGN KEY = 1\n")
+	re = append(re, "SET FOREIGN KEY = 1;")
 	return re
 }
 
